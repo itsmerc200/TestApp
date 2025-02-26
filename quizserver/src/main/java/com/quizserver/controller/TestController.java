87b93c35ce197186a82236d0ea2a6dda7a00fc89
RC <chormalhrushikesh83@gmail.com>
2025-02-27 00:02:19 +0530

    Created a Api to add the question

--- quizserver/src/main/java/com/quizserver/controller/TestController.java
+++ quizserver/src/main/java/com/quizserver/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.quizserver.controller;
 
 
+import com.quizserver.dto.QuestionDTO;
 import com.quizserver.dto.TestDTO;
 import com.quizserver.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,21 @@ public class TestController {
             return new ResponseEntity<>(testService.createtest(dto), HttpStatus.OK);
 
         }catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
+    @PostMapping("/question")
+    public ResponseEntity<?> addQuestionInTest(@RequestBody QuestionDTO dto){
+
+        try {
+
+           return new ResponseEntity<>(testService.addQuestionInTest(dto), HttpStatus.CREATED);
+
+        }catch (Exception e){
+
             return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
         }
 
